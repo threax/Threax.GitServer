55b05b62e5e3fec8497a44c42c1120b613149ab4
Andrew Piper <andrew@threax.com>
2020-08-17 14:22:14 -0400

    Added copy and changed layout of repo results.

--- Threax.GitServer/Views/Home/Index.ts
+++ Threax.GitServer/Views/Home/Index.ts
@@ -2,10 +2,42 @@ import * as standardCrudPage from 'hr.widgets.StandardCrudPage';
 import * as startup from 'clientlibs.startup';
 import * as deepLink from 'hr.deeplink';
 import { GitRepoCrudInjector } from 'clientlibs.GitRepoCrudInjector';
+import { CrudTableRowControllerExtensions, CrudTableRowController } from 'hr.widgets.CrudTableRow';
+import * as controller from 'hr.controller';
+import * as client from 'clientlibs.ServiceClient';
+
+class CrudRow extends CrudTableRowControllerExtensions {
+    private data: client.GitRepoResult;
+
+    public static get InjectorArgs(): controller.DiFunction<any>[] {
+        return [];
+    }
+
+    constructor() {
+        super();
+    }
+
+    public rowConstructed(row: CrudTableRowController, bindings: controller.BindingCollection, data: any): void {
+        bindings.setListener(this);
+        this.data = data;
+    }
+
+    public async copyClonePath(evt: Event): Promise<void>{
+        evt.preventDefault();
+
+        const el = document.createElement('textarea');
+        el.value = this.data.data.clonePath;
+        document.body.appendChild(el);
+        el.select();
+        document.execCommand('copy');
+        document.body.removeChild(el);
+    }
+}
 
 var injector = GitRepoCrudInjector;
 
 var builder = startup.createBuilder();
+builder.Services.tryAddTransient(CrudTableRowControllerExtensions, CrudRow);
 deepLink.addServices(builder.Services);
 standardCrudPage.addServices(builder, injector);
 standardCrudPage.createControllers(builder, new standardCrudPage.Settings());
